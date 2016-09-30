13182c8c4f677f6ac156cc2cb2f0be9bcce3264f
Christopher Luc <chris.luc.dev@gmail.com>
2016-09-30 01:02:53 -0400

    Send endpoint saves to database

--- src/server.go
+++ src/server.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
 	_ "github.com/lib/pq"
+	"log"
 	"net/http"
 	"time"
 )
 
 type Message struct {
-	Text     string
-	Latitude float64
-	Long     float64
-	Expiry   time.Time
+	Text      string
+	Latitude  float64
+	Longitude float64
+	Expiry    time.Time
 }
 
 var db *sql.DB
@@ -21,23 +22,46 @@ func init() {
 	var err error
 	db, err = sql.Open("postgres", "user=goserver dbname=messageinabottle sslmode=disable")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err.Error())
 	}
 }
 
 // Read text, latitude and long from request
 // Set expiry to 24 hours from now
 func sendHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Send Handler")
+	log.Println("Entered Send Handler")
+
+	decoder := json.NewDecoder(r.Body)
+	var message Message
+	err := decoder.Decode(&message)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	t := time.Now()
+	t = t.AddDate(0, 0, 1)
+	_, err = db.Exec(
+		"INSERT INTO message (text, location, expiry) VALUES ($1, point($2, $3), $4)",
+		message.Text,
+		message.Latitude,
+		message.Longitude,
+		t,
+	)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	w.WriteHeader(200)
 }
 
 // Return nearest messages that haven't expired, with all data
 func messagesHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Message Handler")
+	log.Println("Entered Messages Handler")
 
 	rows, err := db.Query("SELECT * FROM message")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err.Error())
 	}
 
 	for rows.Next() {
@@ -45,14 +69,14 @@ func messagesHandler(w http.ResponseWriter, r *http.Request) {
 		var time time.Time
 		err := rows.Scan(&text, &point, &time)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err.Error())
 		}
-		fmt.Println(text, point, time)
+		log.Println(text, point, time)
 	}
 }
 
 func main() {
 	http.HandleFunc("/send/", sendHandler)
 	http.HandleFunc("/messages/", messagesHandler)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
